10889de182ef5df84a9b8ef38540d501fa173700
Erik Marforio <emarforio@gmail.com>
2019-11-28 12:44:42 +0100

    Updated lucka.php

--- src/lucka.php
+++ src/lucka.php
@@ -3,11 +3,13 @@
 // Define year for date check to work in archive later
 define('YEAR', 2019);
 
+$lucka = '';
+
 // Check if parameter "nr" is set and is in the range [1,24]
 if (!isset($_GET['nr']) ||
     $_GET['nr'] < 1 ||
     $_GET['nr'] > 24) {
-  require_once 'luckor/error.html';
+  $lucka = 'error';
 }
 else {
   // Transfer date to local variable for readability
@@ -24,9 +26,34 @@ else {
 
   // If date is before now, return page, otherwise return the peeking page
   if ($now->diff($numberDate)->invert) {
-    require_once 'luckor/' . $_GET['nr'] . '.html';
+    $lucka = $_GET['nr'];
   }
   else {
-    require_once 'luckor/peeking.html';
+    $lucka = 'peeking';
   }
 }
+?>
+<!DOCTYPE html>
+<html lang="sv">
+  <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./vendor/bootstrap/dist/css/bootstrap.min.css">
+
+    <!-- Common styles -->
+    <link rel="stylesheet" href="./css/style.css">
+
+    <!-- Page specific styling -->
+    <link rel="stylesheet" href="./css/lucka.css">
+
+    <title>
+      <?php if ($lucka !== 'error' && $lucka !== 'peeking') echo 'Lucka ' . $lucka . ' '; ?>
+      Julkalendern 2019
+    </title>
+  </head>
+  <body>
+    <?php require 'luckor/' . $lucka . '.html'; ?>
+  </body>
+</html>
