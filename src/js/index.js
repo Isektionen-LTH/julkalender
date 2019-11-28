7800d9e50e6d5396964bf3bdafc5f30715081ee0
David Sandström <david.sandstrom@appberg.se>
2019-11-28 19:45:00 +0100

    header design

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// //var snowf = require('snowf');
+// import snowf from '../vendor/snowf/snowf.js';
+//
+// console.log('SNOW ')
+//
+// snowf.init({
+// 	size: 5,
+// 	amount: 50
+// });
+//
+// module.exports = snowf;
